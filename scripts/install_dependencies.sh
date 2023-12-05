ec9a2bc4f0afc5a80b4045177071b1a14adc4c4e
Francis <fgillet4@gmail.com>
2023-12-05 11:55:04 +0100

    Updates

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -7,13 +7,22 @@ sudo apt-get update
 sudo apt-get upgrade -y
 
 echo "Installing Python3 and pip..."
-sudo apt-get install -y python3 python3-pip
+sudo apt-get install -y python3 python3-pip python3-venv
 
-echo "Installing Python packages required by the project..."
-pip3 install Flask picamera influxdb-client
+echo "Setting up a Python virtual environment for the project..."
+python3 -m venv ~/pi-venv
+
+echo "Activating the virtual environment..."
+source ~/pi-venv/bin/activate
+
+echo "Installing Python packages required by the project within the virtual environment..."
+pip install Flask picamera influxdb-client
+
+echo "Deactivating the virtual environment..."
+deactivate
+
+echo "Installation complete. All necessary Python dependencies are installed in the virtual environment."
 
 echo "Installing InfluxDB (if required by your project)..."
 # Add commands to install InfluxDB here, if it's not already installed.
 # This might vary based on your InfluxDB version and setup requirements.
-
-echo "Installation complete. All necessary dependencies are installed."
